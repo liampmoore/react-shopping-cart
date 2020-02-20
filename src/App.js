28caed83a83af52476724aa998d3cdfbc337714e
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-02-20 18:59:03 -0500

    Added local storage stretch-goal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -13,14 +13,21 @@ import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState(JSON.parse(window.localStorage.getItem('CART')) || []);
 
-	const addItem = item => {
-		setCart([...cart, item])
+	const addItem = newItem => {
+		setCart([...cart, newItem])
 	};
+	const removeItem = item => {
+		setCart(cart.filter(currentItem => currentItem.id !== item.id))
+	}
+
+	useEffect(() => {
+		window.localStorage.setItem('CART', JSON.stringify(cart))
+	}, [cart])
 
 	return (
-		<ProductContext.Provider value={{ products, addItem }}>
+		<ProductContext.Provider value={{ products, addItem, removeItem }}>
 			<CartContext.Provider value={cart}>
 		<div className="App">
 			<Navigation cart={cart} />
