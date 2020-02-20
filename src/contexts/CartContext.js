9468860f2e5681b8c24938a70bff313f318e0817
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-02-20 18:49:15 -0500

    Finished MVP

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,4 +1,5 @@
 import {createContext} from 'react';
 
+const CartContext = createContext()
 
-export default CartContext = createContext()
+export default CartContext
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,4 +1,4 @@
 import {createContext} from 'react';
 
-
-export default ProductContext = createContext()
+const ProductContext = createContext()
+export default ProductContext
